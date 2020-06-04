c15943fba545033715f3661461621a7cd7f7b6b4
Virginie <vbouvarel@lilo.org>
2020-06-04 12:13:00 +0200

    BONUS Mars rover obstacles OK

--- lab03-marsrover/js/app-obstacles.js
+++ lab03-marsrover/js/app-obstacles.js
@@ -6,186 +6,150 @@
 // Rover Object Goes Here
 // ======================
 let rover = {
-    x:0,
-    y:0,
-    direction:"S",
-    move:"forward",
-    travelLog:[]
-  }
+    x: 0,
+    y: 0,
+    direction: "S",
+    travelLog: []
+}
 
 let path = "fffflfffrffffrflfflfffffffff";
 let travel = rover.travelLog;
-  
-let obstacles = [[1,3], [2,7],[4,2],[6,6], [8,1]];
-  
-  // ======================
-  function turnLeft(rover){
-    console.log("turnLeft was called!");
-    switch(rover.direction){
-      case "N":
-        rover.direction = "W";
-        console.log("N -> " + rover.direction);
-        break;
-      case "W":
-        rover.direction = "S";
-        console.log("W -> " + rover.direction);
-        break;
-      case "S":
-        rover.direction = "E";
-        console.log("S -> " + rover.direction);
-        break;
-      case "E":
-        rover.direction = "N";
-        console.log("E -> " + rover.direction);
-        break;
-    }
-  }
-  
-  function turnRight(rover){
-    console.log("turnRight was called!");
-    switch(rover.direction){
-      case "N":
-        rover.direction = "E";
-        console.log("N -> " + rover.direction);
-        break;
-      case "E":
-        rover.direction = "S";
-        console.log("E -> " + rover.direction);
-        break;
-      case "S":
-        rover.direction = "W";
-        console.log("S -> " + rover.direction);
-        break;
-      case "W":
-        rover.direction = "N";
-        console.log("W -> " + rover.direction);
-        break;
-    }
-  }
-  
-  function moveForward(rover){
-    console.log("moveForward was called")
-    if (rover.move === "forward"){
-      switch(rover.direction){
-        case "N":
-          if (rover.y - 1 < 0){
-            console.log("Warning: Off-grid movement");
-          }else{
-            rover.y -= 1;
-            console.log("Rover position: x:" + rover.x + ", y:" + rover.y);
-            travel.push([rover.x,rover.y]);
-          }
-          break;
-        case "E":
-          if (rover.x + 1 > 9){
-            console.log("Warning: Off-grid movement");
-          }else{
-            rover.x += 1;
-            console.log("Rover position: x:" + rover.x + ", y:" + rover.y);
-            travel.push([rover.x,rover.y]);
-          }
-          break;
-        case "S":
-          if (rover.y + 1 > 9){
-            console.log("Warning: Off-grid movement");
-          }else{
-            rover.y += 1;
-            console.log("Rover position: x:" + rover.x + ", y:" + rover.y);
-            travel.push([rover.x,rover.y]);
-          }
-          break;
-        case "W":
-          if (rover.x - 1 < 0){
-            console.log("Warning: Off-grid movement");
-          }else{
-            rover.x -= 1;
-            console.log("Rover position: x:" + rover.x + ", y:" + rover.y);
-            travel.push([rover.x,rover.y]);
-          }
-      }
-    }
-  }
-  
-  function moveBackward(rover){
-    console.log("moveBackward was called")
-    if (rover.move === "backward"){
-      switch(rover.direction){
-        case "N":
-          if (rover.y + 1 > 9){
-            console.log("Warning: Off-grid movement");
-          }else{
-            rover.y += 1;
-            console.log("Rover position: x:" + rover.x + ", y:" + rover.y);
-            travel.push([rover.x,rover.y]);
-          }
-          break;
-        case "E":
-          if (rover.x - 1 < 0){
-            console.log("Warning: Off-grid movement");
-          }else{
-            rover.x -= 1;
-            console.log("Rover position: x:" + rover.x + ", y:" + rover.y);
-            travel.push([rover.x,rover.y]);
-          }
-          break;
-        case "S":
-          if (rover.y - 1 < 0){
-            console.log("Warning: Off-grid movement");
-          }else{
-            rover.y -= 1;
-            console.log("Rover position: x:" + rover.x + ", y:" + rover.y);
-            travel.push([rover.x,rover.y]);
-          }
-          break;
-        case "W":
-          if (rover.x + 1 > 9){
-            console.log("Warning: Off-grid movement");
-          }else{
-            rover.x += 1;
-            console.log("Rover position: x:" + rover.x + ", y:" + rover.y);
-            travel.push([rover.x,rover.y]);
-          }
-      }
-    }
-  }
-  
-  function move(path){
+
+let obstacles = [[3,1], [7,2 ], [2,4], [6,6], [1,8]];
+
+// ======================
+function move(path) {
     //start coordinates
-    travel.push([rover.x,rover.y]);
+    travel.push([rover.x, rover.y]);
     //travel
-    for (let i = 0; i < path.length; i++){
-      switch(path[i]){
-        case "f":
-          rover.move = "forward";
-          moveForward(rover, travel);
-          break;
-        case "b":
-          rover.move = "backward";
-          moveBackward(rover, travel);
-          break;
-        case "r":
-          turnRight(rover, travel);
-          break;
-        case "l":
-          turnLeft(rover, travel);
-          break;
-        default:
-          console.log("invalid command");
-          break;
-      }
+    for (let i = 0; i < path.length; i++) {
+        console.log("move number: " + i);
+        switch (path[i]) {
+            case "f":
+                moveForward(rover);
+                break;
+            case "r":
+                turnRight(rover);
+                break;
+            case "l":
+                turnLeft(rover);
+                break;
+            default:
+                console.log("invalid command");
+                break;
+        }
     }
     //print travel
     console.log("Rover's travel: " + travel);
-  }
-  
-  function verifyNextPosition(nextX,nextY){
-    let isPossible = true;
+}
+function moveForward(rover) {
+    switch (rover.direction) {
+        case "N":
+            nextX = rover.x;
+            nextY = rover.y - 1;
+            nextPosition = verifyNextPosition(nextX, nextY);
+            break;
+        case "E":
+            nextX = rover.x + 1;
+            nextY = rover.y;
+            nextPosition = verifyNextPosition(nextX, nextY);
+            break;
+        case "S":
+            nextX = rover.x;
+            nextY = rover.y + 1;
+            nextPosition = verifyNextPosition(nextX, nextY);
+            break;
+        case "W":
+            nextX = rover.x - 1;
+            nextY = rover.y;
+            nextPosition = verifyNextPosition(nextX, nextY);
+            break;
+    }
+    if (nextPosition){
+        roverGO(nextX, nextY);
+    }
+
+}
+function turnRight(rover) {
+    console.log("Rover turns Right");
+    switch (rover.direction) {
+        case "N":
+            rover.direction = "E";
+            console.log("N -> " + rover.direction);
+            break;
+        case "E":
+            rover.direction = "S";
+            console.log("E -> " + rover.direction);
+            break;
+        case "S":
+            rover.direction = "W";
+            console.log("S -> " + rover.direction);
+            break;
+        case "W":
+            rover.direction = "N";
+            console.log("W -> " + rover.direction);
+            break;
+    }
+}
+function turnLeft(rover) {
+    console.log("Rover turns Left");
+    switch (rover.direction) {
+        case "N":
+            rover.direction = "W";
+            console.log("N -> " + rover.direction);
+            break;
+        case "W":
+            rover.direction = "S";
+            console.log("W -> " + rover.direction);
+            break;
+        case "S":
+            rover.direction = "E";
+            console.log("S -> " + rover.direction);
+            break;
+        case "E":
+            rover.direction = "N";
+            console.log("E -> " + rover.direction);
+            break;
+    }
+}
+
+function verifyNextPosition(nextX, nextY) {
+    let offgrid = isOffgrid(nextX,nextY);
+    let obstacle = isObstacle(nextX,nextY);
+
+    if (offgrid || obstacle){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function isOffgrid(nextX,nextY){
+    if (nextX < 0 || nextX > 9 || nextY < 0 || nextY > 9){
+        console.log("Off-grid:rover doesn't move");
+        return true; 
+    }else{
+        return false;
+    }
+}
+function isObstacle(nextX,nextY){
+    console.log("next case :" + nextX +"/" + nextY);
+    let obstacle = false;
     for (let i = 0; i < obstacles.length; i++){
-      if (nextX === obstacles[i][0] && nextY === obstacles[i][1]){
-        isPossible = false;
-      }
+        if (nextX === obstacles[i][0] && nextY === obstacles[i][1]){
+            console.log("Obstacle case " + obstacles[i][0] + "/" + obstacles[i][1]);
+            console.log("Rover doesn't move, current position:" + rover.x + "/" + rover.y);
+            obstacle = true;
+        }
     }
-    if (isPossible === false){
-      console.log("Warning: Obstacle!");
-    }
-  }
+    return obstacle;
+}
+function roverGO(nextX, nextY){
+    console.log("It's ok, rover move forward");
+    rover.x = nextX;
+    rover.y = nextY;
+    console.log("New position: x:" + rover.x + ", y:" + rover.y);
+    travel.push([rover.x, rover.y]);
+}
+
+move(path);
