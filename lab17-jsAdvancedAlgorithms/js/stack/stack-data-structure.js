daefc309e8d0846085a1a77dd5ad0f2900280591
Virginie <vbouvarel@lilo.org>
2020-07-20 16:06:59 +0200

    add lab 17

---
+++ lab17-jsAdvancedAlgorithms/js/stack/stack-data-structure.js
@@ -0,0 +1,26 @@
+class StackDataStructure {
+  constructor() {
+    this.stackControl = [];
+    this.MAX_SIZE = 10;
+  }
+
+  canPush() {
+    // ... your code goes here
+  }
+
+  display() {
+    // ... your code goes here
+  }
+
+  isEmpty() {
+    // ... your code goes here
+  }
+
+  push(item) {
+    // ... your code goes here
+  }
+
+  pop() {
+    // ... your code goes here
+  }
+}
